2154f48896ed9d0356e9672aafc47c9aebc3084d
avipsha07 <apsarat007@gmail.com>
2025-08-20 22:37:34 +0545

    Update Simple form.py.py

--- Simple form.py.py
+++ Simple form.py.py
@@ -1,18 +1,19 @@
-name = input("Apsara Thapa: ")
-age = int(input("100: "))
-height = float(input("5.6: "))
-country = input("Nepal: ")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in feet: "))
+country = input("Enter your country: ")
 
 
 name_upper = name.upper()
 country_upper = country.upper()
 height = round(height, 2)
 
+
 nickname = (name[0:2] + name[-2:]).upper()
 
 
 print("Hello", name_upper)
-print("Apsara Thapa", age, "100.")
-print("5.6", height, "feet.")
-print("Nepal", country_upper + ".")
-print("APSA", nickname)
+print("You are", age, "years old.")
+print("Your height is", height, "feet.")
+print("You are from", country_upper + ".")
+print("Your nickname is", nickname)
